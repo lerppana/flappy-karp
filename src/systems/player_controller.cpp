8119e21b8a52c7e1815ee503b61ecf186e720227
Lassi Helynranta <lassi@helynranta.com>
2022-10-14 22:13:55 +0300

    implemented splashing

---
+++ src/systems/player_controller.cpp
@@ -0,0 +1,43 @@
+#include "systems/player_controller.hpp"
+
+namespace lerppana::flappykarp::systems
+{
+
+    void player_controller::fixed_update(core::scene& scene, core::dt_t dt)
+    {
+        if (!scene.objects->tagged_has_component<component::physics_3d>(core::tag::player_1))
+        {
+            return;
+        }
+
+        auto& physics_component = scene.objects->get_tagged_component<component::physics_3d>(core::tag::player_1);
+
+        auto splash_reload_delay = std::chrono::milliseconds (400);
+        auto splash_reloaded = last_splash + splash_reload_delay < std::chrono::system_clock::now();
+        if (input_manager->is_key_pressed(SDL_SCANCODE_SPACE) && splash_reloaded)
+        {
+            last_splash = std::chrono::system_clock::now();
+            auto* rigid_body = (btRigidBody*)physics_component.get();
+            rigid_body->setLinearVelocity({0.f, 8.0f, 0.f});
+            rigid_body->setAngularVelocity(
+                    {
+                        util::random_value(-1.0f, 1.f),
+                        util::random_value(-1.0f, 1.f),
+                        util::random_value(-1.0f, 1.f)});
+        }
+    }
+
+    void player_controller::started(core::scene& scene)
+    {
+        scene.physics_world->set_gravity(0.0f, -9.8f, 0.f);
+
+        if (!scene.objects->tagged_has_component<component::physics_3d>(core::tag::player_1))
+        {
+            return;
+        }
+
+        auto& physics_component = scene.objects->get_tagged_component<component::physics_3d>(core::tag::player_1);
+        auto* rigid_body = (btRigidBody*)physics_component.get();
+        rigid_body->setLinearFactor(btVector3{0.f, 1.f, 0.f});
+    }
+}
---
+++ src/systems/player_controller.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <engine.hxx>
+
+namespace lerppana::flappykarp::systems
+{
+    struct player_controller : core::system
+    {
+        explicit player_controller(std::shared_ptr<engine::system::sdl_input_manager> input_manager) :
+            input_manager(std::move(input_manager))
+        {
+        }
+
+        void fixed_update(core::scene& scene, core::dt_t dt) final;
+
+        void started(core::scene& scene) final;
+
+    private:
+        std::shared_ptr<engine::system::sdl_input_manager> input_manager;
+
+        std::chrono::system_clock::time_point last_splash = std::chrono::system_clock::time_point::min();
+    };
+}
