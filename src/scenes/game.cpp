f765ef359cf64e5777e9751a7cbd05f34c295856
Lassi Helynranta <lassi@helynranta.com>
2022-10-21 18:26:46 +0300

    show score

--- src/scenes/game.cpp
+++ src/scenes/game.cpp
@@ -9,22 +9,18 @@ namespace lerppana::flappykarp::scenes
             font_manager->add_font(
                     "thaleah",
                     "fs1://fonts/ThaleahFat.ttf",
-                    128);
+                    64);
         }
     }
 
     void game::on_gui()
     {
         auto* font = font_manager->get_font("thaleah");
-        ImGui::SetWindowFontScale(2.5f);
+        ImGui::SetWindowFontScale(1.0f);
         ImGui::FontScope font_scope{font};
 
         static auto width = 100.0f;
-        auto padding = 100.f;
-        const auto font_height = ImGui::GetFontSize();
-        ImGui::SetCursorPos(ImVec2(
-                ImGui::GetWindowWidth() / 2 - width / 2,
-                ImGui::GetWindowHeight() / 2 - font_height - padding));
+        ImGui::SetCursorPos(ImVec2(ImGui::GetWindowWidth() / 2 - width / 2,0.f));
 
         ImGui::GroupScope text_group;
         ImGui::Text("%d", (uint32_t)score);
---
+++ src/scenes/load.cpp
@@ -0,0 +1,10 @@
+#include "load.hpp"
+
+namespace lerppana::flappykarp::scenes
+{
+    load::load() :
+        engine::core::scene("fs1://scenes/load.scene")
+    {}
+
+    void load::start() {}
+}
